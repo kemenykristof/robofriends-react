b134a93f56cb0dd161321f6f3214f84e1014a556
kemenykristof <kemeny.kristof24@gmail.com>
2019-05-28 16:05:44 +0200

    all cards are showing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './Robots';
 
 ReactDOM.render(
     <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
+        <CardList robots={robots}/>
     </div>,
     document.getElementById('root'));
 
