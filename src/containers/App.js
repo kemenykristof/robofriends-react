0384f4bc4bf4f57574ee920708406da30c9c2f5d
kemenykristof <kemeny.kristof24@gmail.com>
2019-05-30 11:29:40 +0200

    added scrolling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import "./App.css"
 
 
@@ -34,7 +35,9 @@ class App extends Component {
             <div className={'tc'}>
                 <h1 className={"f2"}> Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }
